e27139e3a8638d0e0ae2c552bad77e7402943d87
mattiadido95 <mattia.didonato95@gmail.com>
2025-08-15 19:51:30 +0200

    Initial Commit

---
+++ MoneyTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoneyTracker
+//
+//  Created by Mattia Di Donato on 15/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoneyTracker/MoneyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackerApp.swift
+//  MoneyTracker
+//
+//  Created by Mattia Di Donato on 15/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
